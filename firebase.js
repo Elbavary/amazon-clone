6b28b8379bb842b68862845cce07624463967671
Khaled <khaledgamel2012@gmail.com>
2022-11-12 14:31:24 +0200

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCO7CodYFLbvq4XfTxM-IyHEg3lpQyBwps',
+  authDomain: 'fir-b54f8.firebaseapp.com',
+  projectId: 'fir-b54f8',
+  storageBucket: 'fir-b54f8.appspot.com',
+  messagingSenderId: '725647916321',
+  appId: '1:725647916321:web:12a3e33b6209dc13411260',
+};
